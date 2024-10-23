20250046541b2e5353bcdb52e11f0622b6313fb6
Frido Afriyanto <146181205+fridoa@users.noreply.github.com>
2024-10-24 01:19:56 +0700

    Update main.dart

--- 06_Interaksi_Pengguna/GUIDED/main.dart
+++ 06_Interaksi_Pengguna/GUIDED/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:guided/my_navbar.dart';
+import 'package:guided/my_package.dart';
+import 'package:guided/my_tabbar.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '2211104088',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: //myPackage(),
+          //myTabbar(),
+          myNavbar(),
+    );
+  }
+}
