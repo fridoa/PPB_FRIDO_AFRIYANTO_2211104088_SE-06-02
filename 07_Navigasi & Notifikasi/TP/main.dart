620452b3446aab719e9a4cbccf490bb973112baa
Frido Afriyanto <146181205+fridoa@users.noreply.github.com>
2024-10-29 17:03:25 +0700

    Update main.dart

--- 07_Navigasi & Notifikasi/TP/main.dart
+++ 07_Navigasi & Notifikasi/TP/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'second_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // Menentukan home screen aplikasi
+      home: HomePage(),
+      // Mendefinisikan rute aplikasi
+      routes: {
+        '/second': (context) => SecondPage(),
+      },
+    );
+  }
+}
